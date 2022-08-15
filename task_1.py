5f1b249c51d09f529e88f388509a108976f58fc0
Alexunder Pakhomchik <korsar7772@gmail.com>
2022-08-15 15:58:49 +0300

    remake class Version

--- task_1.py
+++ task_1.py
@@ -1,6 +1,28 @@
-from dataclasses import dataclass
-
-@dataclass(order = True, frozen = True)
+import re
 
 class Version:
-    version: str
+    def __init__(self, version: str):
+        self.version = self.set_version(version)
+    def __str__(self):
+        return f'Версия: {self.version}'
+    @staticmethod
+    def set_version(version):
+        if re.match(r'[0-9].[0-9].[0-9]', version) is None:
+            raise ValueError('Формат: X.Y.Z')
+        else:
+            return version
+
+    def __lt__(self, other):
+        return self.version < other.version
+
+    def __le__(self, other):
+        return self.version <= other.version
+
+    def __eq__(self, other):
+        return self.version == other.version
+
+    def __gt__(self, other):
+        return self.version > other.version
+
+    def __ge__(self, other):
+        return self.version >= self.other
